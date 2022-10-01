e0bf3d0aa4ae2aa12f8c6c4a9cf82b6d97303743
Abdulrhman <abdomet66@gmail.com>
2022-10-01 13:32:43 +0200

    done

--- backend/src/modules/domain/employees/commands/handlers/employee-activator.handler.spec.ts
+++ backend/src/modules/domain/employees/commands/handlers/employee-activator.handler.spec.ts
@@ -19,7 +19,7 @@ describe('Employee Remover', () => {
       const handler = new EmployeeActivator(employeeRepository);
 
       const params = {
-        employeeId: 101, //change this to 100 to make the test pass
+        employeeId: 100, //change this to 100 to make the test pass
         isActive: false,
       };
 
@@ -31,9 +31,10 @@ describe('Employee Remover', () => {
       // Act
       await handler.handle(activateEmployeeCommand);
 
-      // Assert
-      expect(employeeRepository.findById).toBeCalledWith(100);
-      expect(employeeRepository.save).toBeCalled();
+     // Assert
+     expect(employeeRepository.findById).toBeCalledWith(100);
+     expect(employeeRepository.save).toBeCalled();
+
     });
   });
 });
